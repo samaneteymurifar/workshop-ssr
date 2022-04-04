ea769bb6dab438c910405fb0d529954d441054e7
samane26 <samane.teymurifar@gmail.com>
2022-04-04 20:41:09 +0430

    1 add

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 const Home= () => {
     return (
         <div>   
-         <div>I am home samane from somaye</div>
+         <div>I am home samane from somaye 1</div>
          <button onClick={()=>console.log("hello world")}>press mee</button>
         </div>
 
